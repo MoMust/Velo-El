1f3a5905dde037490cc190e8e32e96d20939d048
Mohammd Mustafa <momust187@gmail.com>
2023-04-13 16:31:49 +0200

    Pages and first routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import './App.css';
-import AccessibilityNewIcon from "@mui/icons-material/AccessibilityNew";
-import Button from "@mui/material/Button";
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Outlet
+} from "react-router-dom"
+
+// import Layout from "./pages/Layout";
+import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
+import Product from './pages/Product/Product';
+import Service from './pages/_Service/Service';
+import Contact from './pages/Contact/Contact';
+import About from './pages/About/About';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/products/:id",
+    element: <Products />
+  },
+  {
+    path: "/product/:id",
+    element: <Product />
+  },
+  {
+    path: "/service",
+    element: <Service />
+  },
+  {
+    path: "/contact",
+    element: <Contact />
+  },
+  {
+    path: "/about",
+    element: <About />
+  },
+]);
 
 function App() {
   return (
     <div>
-     <h1 style={{fontWeight: '600'}}>Is working</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='Home'>
+        <div>Home</div>
+    </div>
+  )
+}
+
+export default Home
