78fb27d99525b016b64cd97e47941b386dc3c381
Mohammd Mustafa <momust187@gmail.com>
2023-05-02 11:54:51 +0200

    added spec info

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -7,18 +7,19 @@ import useFetch from "../../common/hooks/useFetch";
 const Product = () => {
   const id = useParams().id;
 
+
   const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
   console.log("product ", data);
 
+  let array = [];
+
+  
+
+  array = data?.attributes?.spec;
+  console.log(array)
   const [selectedImg, setSelectedImg] = useState("img");
   const [quantity, setQuantity] = useState(1);
-  // console.log("id ", id)
-
-  // const Images = [
-  //   "../assets/images/CT20/CT20-1.webp",
-  //   "../assets/images/CT20/2-1.webp"
-
-  // ]
+  
 
   return (
     <div className="row product d-flex m-0 pt-3 pb-3 ps-2 pr-5 gap">
@@ -93,7 +94,81 @@ const Product = () => {
                 <AddShoppingCartIcon /> ADD TO CART
               </button>
             </div>
-          </div>{" "}
+          </div>
+          <div className="spec">
+            <h1>Specifikation</h1>
+            <ul className="spec-ul p-4 mt-4">
+              <li>
+                <span>Färg:</span> {data?.attributes?.spec?.color}
+              </li>
+              <li>
+                <span>COC godkänd:</span> {data?.attributes?.spec?.COC}
+              </li>
+              <li>
+                <span>Motor:</span> {data?.attributes?.spec?.motor}
+              </li>
+              <li>
+                <span>Hastighet:</span> {data?.attributes?.spec?.speed}
+              </li>
+              <li>
+                <span>Laddning:</span> {data?.attributes?.spec?.charge}
+              </li>
+              <li>
+                <span>Batteri:</span> {data?.attributes?.spec?.battery}
+              </li>
+              <li>
+                <span>Totalavikt:</span> {data?.attributes?.spec?.weight}
+              </li>
+              <li>
+                <span>Maxvikt förare:</span> {data?.attributes?.spec?.maxweight}
+              </li>
+              <li>
+                <span>Display:</span> {data?.attributes?.spec?.display}
+              </li>
+              <li>
+                <span>Räckvidd:</span> {data?.attributes?.spec?.distance}
+              </li>
+              <li>
+                <span>Däck:</span> {data?.attributes?.spec?.wheels}
+              </li>
+              <li>
+                <span>Material:</span> {data?.attributes?.spec?.material}
+              </li>
+              <li>
+                <span>Bromsar:</span> {data?.attributes?.spec?.brakes}
+              </li>
+              <li>
+                <span>Körlägen:</span> {data?.attributes?.spec?.drivingmode}
+              </li>
+              <li>
+                <span>Belysning:</span> {data?.attributes?.spec?.lights}
+              </li>
+              <li>
+                <span>Elektronisk hastighetskontroll:</span> {data?.attributes?.spec?.speedcontrol}
+              </li>
+              <li>
+                <span>Hastighetsjustering:</span> {data?.attributes?.spec?.speedadjustment}
+              </li>
+              <li>
+                <span>Suspension/fjädrad:</span> {data?.attributes?.spec?.suspension}
+              </li>
+              <li>
+                <span>Garanti:</span> {data?.attributes?.spec?.guarantee}
+              </li>
+              <li>
+                <span>Storlek:</span> {data?.attributes?.spec?.size}
+              </li>
+              <li>
+                <span>Förpackningsstorlek:</span> {data?.attributes?.spec?.packsize}
+              </li>
+              <li>
+                <span>Försäkring:</span> {data?.attributes?.spec?.insurance}
+              </li>
+              <li>
+                <span>Ingår med:</span> {data?.attributes?.spec?.include}
+              </li>
+            </ul>
+          </div>
         </>
       )}
     </div>
