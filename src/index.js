6a69978840e9c3a332d5354430f014bf49f06666
Mohammd Mustafa <momust187@gmail.com>
2023-05-07 14:23:07 +0200

    using redux-presist to keep cart from reseting on page refresh

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.css";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={"loading"} persistor={persistor}>
     <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
