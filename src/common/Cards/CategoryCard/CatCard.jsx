a6c0cc11249c07a2fbf501bb7fb4c2c1f3cf910b
Mohammd Mustafa <momust187@gmail.com>
2023-07-26 20:33:06 +0200

    change of component name, card

--- src/common/Cards/CategoryCard/CatCard.jsx
+++ src/common/Cards/CategoryCard/CatCard.jsx
@@ -3,11 +3,13 @@ import "./CatCard.scss";
 import { Link } from "react-router-dom";
 // import useFetch from "../hooks/useFetch"
 
-const Card = ({ product }) => {
+const CatCard = ({ product }) => {
+  console.log("check", product)
   return (
-    <div className="styled-card d-flex justify-content-center flex-column">
-      <div className="d-flex justify-content-center">
+    <div className="styled-card pt-5 d-flex justify-content-center flex-column">
+      <div className=" d-flex justify-content-center p-4">
         <Link to={`/product/${product?.id}`}>
+          {/* How to make an image with style of 100 width and length */}
           <img
             className="card-img-top"
             src={
@@ -20,9 +22,9 @@ const Card = ({ product }) => {
       </div>
 
       <div className="card-body  d-flex justify-content-center">
-        <div className="item-info text-center">
-          <p className="card-text">{product?.attributes?.title}</p>
-          <div className="price d-flex">
+        <div className="item-info  gap-2">
+          <p className="card-text text-center">{product?.attributes?.title}</p>
+          <div className="price d-flex d-flex justify-content-center ">
             <p
               className="card-text fw-light"
               style={{ textDecoration: "line-through" }}
@@ -31,10 +33,30 @@ const Card = ({ product }) => {
             </p>
             <p className="card-text fw-normal">{`${product?.attributes.price} SEK`}</p>
           </div>
+          <div className="product-att pb-5 d-flex justify-content-center gap-3">
+            <div>
+              <p className="fs-6 m-0">Färg:</p>
+              <p className="fw-light fs-6  m-0">
+                {product?.attributes?.spec?.color}
+              </p>
+            </div>
+            <div>
+              <p className=" fs-6  m-0">Hastighet:</p>
+              <p className="fw-light fs-6  m-0">
+                {product?.attributes?.spec?.speed}
+              </p>
+            </div>
+            <div>
+              <p className=" fs-6  m-0">Batteri:</p>
+              <p className="fw-light fs-6  m-0">
+                {product?.attributes?.spec?.battery}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default Card;
+export default CatCard;
